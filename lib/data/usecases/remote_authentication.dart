dcc487ff40b183f43a27675cc4b2068f5a1ed3ad
Leandro Pinto <leandro.pnto@gmail.com>
2022-05-13 20:40:11 -0300

    Seção 2 - Aula 10: Testando caso de sucesso.

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,3 +1,5 @@
+import 'package:enquetes/data/models/models.dart';
+import 'package:enquetes/domain/entities/account_entity.dart';
 import 'package:enquetes/domain/helpers/domain_error.dart';
 
 import '../../domain/usecases/usecases.dart';
@@ -9,17 +11,20 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void> auth(AuthenticationParams params) async {
+  Future<AccountEntity> auth(AuthenticationParams params) async {
     try {
-      await httpClient.request(
+      final httpResponse = await httpClient.request(
         url: url,
         method: 'post',
         body: RemoteAuthenticationParams.fromDomain(params).toJson(),
       );
+      return RemoteAccountModel.fromJson(httpResponse).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.unauthorized
           ? DomainError.invalidCredentials
           : DomainError.unexpected;
+    } on Error {
+      throw DomainError.unexpected;
     }
   }
 }
@@ -28,8 +33,7 @@ class RemoteAuthenticationParams {
   final String email;
   final String password;
 
-  const RemoteAuthenticationParams(
-      {required this.email, required this.password});
+  const RemoteAuthenticationParams({required this.email, required this.password});
 
   factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
       RemoteAuthenticationParams(email: params.email, password: params.secret);
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -3,8 +3,8 @@ import 'package:enquetes/data/usecases/usecases.dart';
 import 'package:enquetes/domain/helpers/helpers.dart';
 import 'package:enquetes/domain/usecases/authentication.dart';
 import 'package:faker/faker.dart';
-import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {
   When mockRequestCall() => when(() => request(
@@ -14,9 +14,11 @@ class HttpClientSpy extends Mock implements HttpClient {
         /*headers: any(named: 'headers')*/
       ));
 
-  void mockRequest() => mockRequestCall().thenAnswer((invocation) async {});
+  //void mockRequest() => mockRequestCall().thenAnswer((invocation) async {});
 
-//void mockRequest(dynamic data) => mockRequestCall().thenAnswer((_) async => data);
+  void mockRequest(dynamic data) =>
+      mockRequestCall().thenAnswer((_) async => data);
+
   void mockRequestError(HttpError error) => mockRequestCall().thenThrow(error);
 }
 
@@ -25,10 +27,16 @@ void main() {
   late String url;
   late RemoteAuthentication sut;
   late AuthenticationParams params;
+  late String token;
+  late String name;
+  late Map result;
 
   setUp(() {
     httpClient = HttpClientSpy();
-    httpClient.mockRequest();
+    token = faker.guid.guid();
+    name = faker.person.name();
+    result = {'accessToken': token, 'name': name};
+    httpClient.mockRequest(result);
 
     params = AuthenticationParams(
       email: faker.internet.email(),
@@ -72,7 +80,6 @@ void main() {
 
     final future = sut.auth(params);
     expect(future, throwsA(DomainError.unexpected));
-
   });
 
   test('Should throw Unexpected error if httpClient returns 401', () async {
@@ -80,7 +87,21 @@ void main() {
 
     final future = sut.auth(params);
     expect(future, throwsA(DomainError.invalidCredentials));
-
   });
 
+  test('Should return a account when HttpClient returns 200', () async {
+    httpClient.mockRequest(result);
+
+    final account = await sut.auth(params);
+    expect(account.token, token);
+  });
+
+  test(
+      'Should throws UnexpectedError if HttpClient returns 200 with invalid data',
+      () async {
+    httpClient.mockRequest({'invalid_key': 'invalid_data'});
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
